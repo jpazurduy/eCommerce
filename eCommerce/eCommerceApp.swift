bd7834c9ac03c151c412203b7f2c285ee60d02c7
Jorge Azurduy <jpa.azurduy@gmail.com>
2026-01-20 10:58:37 -0400

    base

---
+++ eCommerce/eCommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceApp.swift
+//  eCommerce
+//
+//  Created by Jorge Azurduy on 20/1/26.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ eCommerceTests/eCommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceTests.swift
+//  eCommerceTests
+//
+//  Created by Jorge Azurduy on 20/1/26.
+//
+
+import Testing
+@testable import eCommerce
+
+struct eCommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
